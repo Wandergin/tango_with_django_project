d11202ba6d69327f46f7253329a2f9effb1f2abe
MariusVin <m.vinickis@gmail.com>
2016-02-10 15:08:47 +0000

    Flush

--- rango/urls.py
+++ rango/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, url
 from rango import views
 
 urlpatterns = patterns('',
-        url(r'rango^$', views.index, name='index'))
+        url(r'^$', views.index, name='index'),
+	    url(r'^about/$', views.about, name='about'),
+	    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),)  # New!
